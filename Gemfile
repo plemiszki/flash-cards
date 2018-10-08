ac060b8bfc04b6157dbb7f450891f1f5e8d106ac
Peter Lemiszki <plemiszki@gmail.com>
2018-10-08 10:17:32 -0400

    create users

--- Gemfile
+++ Gemfile
@@ -10,16 +10,15 @@ gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap-sass'
 gem 'clearance', '~> 1.11'
+gem 'pry-rails'
 
 group :development, :test do
   gem 'byebug', platform: :mri
-  gem 'pry-rails'
   gem 'better_errors'
   gem 'dotenv-rails' # load environment variables from .env file
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180909224522) do
+ActiveRecord::Schema.define(version: 20181008141149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20180909224522) do
     t.datetime "updated_at",            null: false
     t.string   "transliterated"
     t.string   "transliterated_plural"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
