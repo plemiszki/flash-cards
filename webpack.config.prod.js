2decf5dd6463401399201600741359523c6636ae
Peter Lemiszki <plemiszki@gmail.com>
2018-10-10 20:20:30 -0400

    prep for heroku

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  context: __dirname,
+  entry: './flux/entry.jsx',
+  output: {
+    path: __dirname + '/app/assets/javascripts/me',
+    filename: 'bundle.js',
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
