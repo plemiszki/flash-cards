b20cdf4bbe1613d349fe2d1c7f8a20ae72dd2254
Peter Lemiszki <plemiszki@gmail.com>
2019-09-03 20:00:37 -0400

    fix needs attention tag bug

--- app/controllers/api/adjectives_controller.rb
+++ app/controllers/api/adjectives_controller.rb
@@ -8,7 +8,7 @@ class Api::AdjectivesController < AdminController
   def create
     @adjective = Adjective.new(adjective_params)
     if @adjective.save
-      if params[:adjective][:needs_attention]
+      if params[:adjective][:needs_attention] == "true"
         tag_id = Tag.find_by_name('Needs Attention').id
         CardTag.create(cardtagable_type: 'Adjective', cardtagable_id: @adjective.id, tag_id: tag_id)
       end
