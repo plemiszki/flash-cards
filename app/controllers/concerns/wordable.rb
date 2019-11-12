562316710556452b1d906ef1fadced5f71074a15
Peter Lemiszki <plemiszki@gmail.com>
2019-11-12 10:37:29 -0500

    fix remove_needs_attention method

--- app/controllers/concerns/wordable.rb
+++ app/controllers/concerns/wordable.rb
@@ -5,7 +5,8 @@ module Wordable
     entity = @noun || @verb || @adjective || @spanish_noun || @spanish_verb || @spanish_adjective
     entity_name = controller_name.camelize[0...-1]
     if entity.streak >= 5
-      tag = CardTag.find_by(name: 'Archived', cardtagable_id: entity.id, cardtagable_type: entity_name)
+      needs_attention_tag_id = Tag.find_by(name: 'Needs Attention')
+      tag = CardTag.find_by(tag_id: needs_attention_tag_id, cardtagable_id: entity.id, cardtagable_type: entity_name)
       tag.destroy if tag
     end
   end
