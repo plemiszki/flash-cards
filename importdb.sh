107ada6e301493d74496f8fab93d78fb0e4181da
Peter Lemiszki <plemiszki@gmail.com>
2020-07-11 11:52:04 -0400

    update import script

--- importdb.sh
+++ importdb.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-rm latest.dump
+
+if [ -f latest.dump ]; then
+  rm latest.dump
+fi
+
 heroku pg:backups:capture --app flash-cardz
 heroku pg:backups:download --app flash-cardz
+
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d flash-cards_development latest.dump
 rm latest.dump
+
 echo Production Database Imported!
