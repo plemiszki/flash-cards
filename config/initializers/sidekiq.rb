09c7b9811ea2552a0737422fe29dad78336d6b03
Peter Lemiszki <plemiszki@gmail.com>
2024-11-10 13:54:32 -0500

    remove commented code

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    # url: ENV["REDIS_TLS_URL"],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-      # url: ENV["REDIS_TLS_URL"],
       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
