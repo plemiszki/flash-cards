50fb103a05511039fe717efb9e143035f6cbc9ef
Peter Lemiszki <plemiszki@gmail.com>
2026-02-22 17:37:52 -0500

    fix script

--- scripts/backfill_quiz_question_tags.rb
+++ scripts/backfill_quiz_question_tags.rb
@@ -1,7 +1,8 @@
 QuizQuestion.where.not(tag_id: nil).find_each do |quiz_question|
   next if QuizQuestionTag.exists?(quiz_question_id: quiz_question.id, tag_id: quiz_question.tag_id)
 
-  QuizQuestionTag.create!(quiz_question_id: quiz_question.id, tag_id: quiz_question.tag_id)
+  tag = quiz_question.tag
+  QuizQuestionTag.create!(quiz_question_id: quiz_question.id, tag: tag) unless tag.nil?
 end
 
 puts "Done."
