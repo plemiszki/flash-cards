9dbfef7a6179080d48bf5246cc8e89aa2d47d6cd
Peter Lemiszki <plemiszki@gmail.com>
2026-03-05 13:23:32 -0500

    backfill script

--- scripts/backfill_quiz_question_type.rb
+++ scripts/backfill_quiz_question_type.rb
@@ -3,3 +3,11 @@ QuizQuestion.where(use_all_available: true).find_each do |quiz_question|
 end
 
 puts "Done."
+
+# Backfill: change all_non_archived to all_highlighted
+count = QuizQuestion.all_non_archived.count
+puts "Found #{count} QuizQuestion record(s) with type all_non_archived"
+
+QuizQuestion.all_non_archived.update_all(quiz_question_type: :all_highlighted)
+
+puts "Updated #{count} record(s) to all_highlighted"
