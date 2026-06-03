e81b604758011e45ee38fc31eafb2a50838b7d57
Peter Lemiszki <plemiszki@gmail.com>
2026-06-03 12:55:46 -0400

    match bins - order by name

--- app/controllers/api/match_bins_controller.rb
+++ app/controllers/api/match_bins_controller.rb
@@ -3,7 +3,7 @@ class Api::MatchBinsController < AdminController
   def create
     match_bin = MatchBin.new(match_bin_params)
     if match_bin.save
-      @match_bins = MatchBin.where(card_id: match_bin.card_id)
+      @match_bins = MatchBin.where(card_id: match_bin.card_id).order(:name)
       render 'index', formats: [:json], handlers: [:jbuilder]
     else
       render json: match_bin.errors.full_messages, status: 422
@@ -13,14 +13,14 @@ class Api::MatchBinsController < AdminController
   def update
     match_bin = MatchBin.find(params[:id])
     match_bin.update!(match_bin_params)
-    @match_bins = MatchBin.where(card_id: match_bin.card_id)
+    @match_bins = MatchBin.where(card_id: match_bin.card_id).order(:name)
     render 'index', formats: [:json], handlers: [:jbuilder]
   end
 
   def destroy
     match_bin = MatchBin.find(params[:id])
     match_bin.destroy
-    @match_bins = MatchBin.where(card_id: match_bin.card_id)
+    @match_bins = MatchBin.where(card_id: match_bin.card_id).order(:name)
     render 'index', formats: [:json], handlers: [:jbuilder]
   end
 
